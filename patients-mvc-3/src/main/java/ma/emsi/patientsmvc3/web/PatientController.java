e8276c4c950c9c9c5886bef97386d25aab191452
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-23 16:32:06 +0100

    Ajoute du Spring Secrity

---
+++ patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/web/PatientController.java
@@ -0,0 +1,82 @@
+package ma.emsi.patientsmvc3.web;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import ma.emsi.patientsmvc3.entities.Patient;
+import ma.emsi.patientsmvc3.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+    @GetMapping(path="/index")
+    public String index(Model model ,
+                                    @RequestParam(name="page",defaultValue = "0") int page ,
+                                    @RequestParam(name="size",defaultValue = "5")  int size ,
+                                    @RequestParam(name="keyWord", defaultValue = "") String keyWord)
+    {
+        Page<Patient> pagePatients=patientRepository.findByNomContains(keyWord,PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyWord",keyWord);
+        //retourne une vue appele patients.html
+        return "patients";
+    }
+    @GetMapping("/delete")
+    public String delete(Long id, String keyWord, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyWord="+keyWord;
+    }
+
+    @GetMapping("/addPatients")
+    public String addPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return "addPatients";
+    }
+
+    @GetMapping("/editPatients")
+    public String editPatients(Model model ,Long id ,String keyWord ,int page){
+    Patient patient=patientRepository.findById(id).orElse(null);
+    if(patient==null) throw new RuntimeException("Patient introuvable");
+    model.addAttribute("patient",patient);
+    model.addAttribute("page",page);
+    model.addAttribute("keyWord",keyWord);
+    return "editPatients";
+    }
+
+    @PostMapping(path="/save")
+    public String save(Model mode,
+                       @Valid Patient patient ,
+                       BindingResult bindingResult ,
+                       @RequestParam (defaultValue = "0") int page ,
+                       @RequestParam (defaultValue = "") String keyWord){
+        if(bindingResult.hasErrors())
+            return "addPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyWord="+keyWord;
+    }
+
+    @GetMapping("/patientsJson")
+    @ResponseBody
+    public List<Patient> listePatients(){
+        return patientRepository.findAll();
+    }
+}
