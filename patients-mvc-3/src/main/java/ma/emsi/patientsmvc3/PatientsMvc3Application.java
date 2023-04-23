f8509a526876336abcfc838e2e0c99015479ff2b
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-23 14:20:55 +0100

    first commit

---
+++ patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/PatientsMvc3Application.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientsmvc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsMvc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvc3Application.class, args);
+    }
+
+}
