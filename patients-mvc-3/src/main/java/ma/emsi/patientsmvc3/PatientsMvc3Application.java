d303d81a10f6666ca2230d928cfd7ab314692a33
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-27 00:12:18 +0100

    integration of passwordEncoder

--- patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/PatientsMvc3Application.java
+++ patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/PatientsMvc3Application.java
@@ -2,12 +2,20 @@ package ma.emsi.patientsmvc3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PatientsMvc3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(PatientsMvc3Application.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
