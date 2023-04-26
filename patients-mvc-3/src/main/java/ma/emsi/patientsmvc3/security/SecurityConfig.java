070f9d23d79ab7af181f69845c741e4c8b7943c9
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-27 00:01:17 +0100

    inMemoryUserDetailsManager function

--- patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/security/SecurityConfig.java
+++ patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/security/SecurityConfig.java
@@ -4,11 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").roles("USER").build(),
+                User.withUsername("user2").password("{noop}1234").roles("USER").build(),
+                User.withUsername("admin").password("{noop}1234").roles("ADMIN","USER").build()
+
+        );
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.formLogin();
