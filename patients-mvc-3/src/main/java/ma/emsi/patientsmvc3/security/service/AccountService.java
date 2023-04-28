b178ca58bd6063a4c2a0a617e71b0d32ae02b66f
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-28 17:42:29 +0100

    adding security.service package that contains the Interface AccountService and the class AccountServiceImpl

---
+++ patients-mvc-3/src/main/java/ma/emsi/patientsmvc3/security/service/AccountService.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientsmvc3.security.service;
+
+import ma.emsi.patientsmvc3.security.entities.AppRole;
+import ma.emsi.patientsmvc3.security.entities.AppUser;
+
+public interface AccountService {
+    AppUser addNewUser(String username, String password , String email, String confirmPassword);
+    AppRole addNewRole(String role);
+    void addRoleToUser(String username, String role);
+    void removeRoleFromUser(String username, String role);
+    AppUser loadUserByUsername(String username);
+
+}
